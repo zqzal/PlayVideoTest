eeae80ead3b8352951f067eef9c8e6788204666f
zqzal <936952020@qq.com>
2019-08-15 19:51:03 +0800

    playvideo

--- app/src/main/java/com/xuxi/playvideotest/MainActivity.java
+++ app/src/main/java/com/xuxi/playvideotest/MainActivity.java
@@ -1,14 +1,94 @@
 package com.xuxi.playvideotest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.video_view);
+        Button play = findViewById(R.id.play);
+        Button pause = findViewById(R.id.pause);
+        Button replay = findViewById(R.id.replay);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        replay.setOnClickListener(this);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        }else {
+            initVideoPath(); //初始化VideoView
+        }
+
+    }
+
+    private void initVideoPath(){
+        File file = new File(Environment.getExternalStorageDirectory(),"movie.mp4");
+        videoView.setVideoPath(file.getPath()); //指定视频文件的路径
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    initVideoPath();
+                }else {
+                    Toast.makeText(this,"拒绝权限将无法使用程序",Toast.LENGTH_SHORT).show();
+                }
+                break;
+                default:
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.play:
+                if (!videoView.isPlaying()){
+                    videoView.start(); //开始播放
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()){
+                    videoView.pause(); //暂停播放
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying()){
+                    videoView.resume(); //重新播放
+                }
+                break;
+
+        }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null){
+            videoView.suspend();
+        }
     }
 }
